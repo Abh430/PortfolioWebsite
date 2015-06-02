64f4381968e42f0dd34043797f392bab685b8f2f
Abh430 <abh430@gmail.com>
2015-06-02 14:45:23 -0400

    updated styles

--- js/master.js
+++ js/master.js
@@ -7,36 +7,28 @@ $(document).ready(function() {
     var windScroll = $(window).scrollTop();
     if (windScroll >= 0) {
         $('#main article').each(function(i) {
-            if ($(this).position().top <= windScroll - 100) {
-                $('nav a.active').removeClass('active');
-                $('nav a').eq(i).addClass('active');
+            if ($(this).position().top <= windScroll + 100) {
+                $('.nav-header a.active').removeClass('active');
+                $('.nav-header a').eq(i).addClass('active');
             }
         });
 
     } else {
 
-        $('nav a.active').removeClass('active');
-        $('nav a:first').addClass('active');
+        $('.nav-header a.active').removeClass('active');
+        $('.nav-header a:first').addClass('active');
     }
 
 	});
 	
 // scroll animations ======================================
 
-	$('nav a').click(function() {
+	$('.nav-header a').click(function() {
 		var anchor = $(this);
 		$('body, html').stop().animate({scrollTop: $(anchor.attr('href')).offset().top}, 1200, 'easeInOutExpo');
 		return false;
 	});
 	
-	$('.popup-gallery').each(function() {
-		$(this).magnificPopup({
-  		delegate: 'a', // child items selector, by clicking on it popup will open
-  		type: 'image',
-  		gallery:{enabled:true},
-  		preload: [1,3],
-  		});
-	});
 
   // Project image dividers =====================================
 
